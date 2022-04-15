10dc620b87b1e40fcf302c3882ac3e4863b20c6c
Juan Maxwell Noronha <juanmnoronha@gmail.com>
2022-04-15 20:04:20 -0300

    feat: add HTML structure

--- src/pages/App/App.style.ts
+++ src/pages/App/App.style.ts
@@ -1,5 +1,9 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
+  align-items: flex-start;
   display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding: 48px;
 `
--- src/pages/App/App.tsx
+++ src/pages/App/App.tsx
@@ -4,6 +4,27 @@ import { Container } from './App.style';
 
 export function App() {
   return (
-    <Container />
+    <Container>
+      <section>
+        <header>
+          <h1>Your pipes</h1>
+          <span>Help</span>
+          <p>Here are all your process <a href="/">learn more</a>.</p>
+        </header>
+        <div>
+          <div>
+            <div>
+              <i>Lock</i>
+              <i>Favorite</i>
+              <h2>Title</h2>
+              <span>
+                <span>0</span>
+                Cards
+              </span>
+            </div>
+          </div>
+        </div>
+      </section>
+    </Container>
   );
 }
