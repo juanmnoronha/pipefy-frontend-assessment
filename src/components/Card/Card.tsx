56b3f6b45075801f21fabc4569e2a71ed1c3c7cc
Juan Maxwell Noronha <juanmnoronha@gmail.com>
2022-04-20 12:17:46 -0300

    fix: add image default on error

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { ReactEventHandler } from 'react';
 
 import { TEMPLATE_ICONS_PATH } from '../../utils/constants';
 import * as S from './Card.style';
@@ -9,11 +9,17 @@ interface CardProps {
   icon: string
   isPublic: boolean
   onClick: () => void
+  onError: ReactEventHandler
   title: string
 }
 
 export function Card({ color, title, icon, count, onClick, isPublic }: CardProps) {
   const iconPath = `${TEMPLATE_ICONS_PATH}${icon}-xl.svg`;
+  const iconDefaultPath = `${TEMPLATE_ICONS_PATH}pipefy-solid-xl.svg`;
+
+  const handleImgError = (event: any) => {
+    event.target.src = iconDefaultPath
+  }
 
   return (
     <S.Container color={color} onClick={onClick}>
@@ -21,7 +27,7 @@ export function Card({ color, title, icon, count, onClick, isPublic }: CardProps
         {!isPublic && <S.LockIcon />}
         <S.FavoriteIcon />
       </S.Actions>
-      <S.Icon src={iconPath} alt='' />
+      <S.Icon src={iconPath} onError={handleImgError} alt={title} />
       <S.Content>
         <S.Title>{title}</S.Title>
         <S.Count>{count} Cards</S.Count>
